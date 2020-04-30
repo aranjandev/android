4fea27ea7a70eae5588dcb2a94e4844c8a65cee3
Abhishek Ranjan <abhishek.ranjan@gmail.com>
2020-04-30 13:54:39 -0400

    Consolidated functions and datastructures to make easier to handle different sensors and allowing sensor handling just using Sensor.TYPE_* int.

--- BasicWearOS/app/src/main/java/com/example/basicwearos/MainActivity.java
+++ BasicWearOS/app/src/main/java/com/example/basicwearos/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.basicwearos;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
@@ -9,9 +8,10 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
+import android.util.Pair;
 
-import java.util.Arrays;
 import java.util.Vector;
+import java.util.HashMap;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -21,86 +21,83 @@ public class MainActivity extends WearableActivity implements SensorEventListene
 
     private TextView mTextView;
     private static final String TAG = "MyActivity";
-    private String mMesgAcc = "Acc: ";
-    private String mMesgGyr = "Gyr:";
-    private String mMesgGra = "Gra:";
-    private String mMesgHR = "HR:";
+    private HashMap sensorMessages;
+    private HashMap sensorMessagePrefix;
     private static final int Q_SZ = 500;
-    private EvictingQueue<Float> mAccXQ, mAccYQ, mAccZQ;
-    private EvictingQueue<Float> mGyrXQ, mGyrYQ, mGyrZQ;
-    private EvictingQueue<Float> mGvtXQ, mGvtYQ, mGvtZQ;
-    DataSampleProcessor mDProc;
+    // Map to store the starting Q index and number of Qs for a given sensor
+    private HashMap sensorDataIndex;
+    private Vector<EvictingQueue<Float>> allDataQ;
+//    DataSampleProcessor mDProc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        mDProc = new DataSampleProcessor();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextView = (TextView) findViewById(R.id.text);
-
-        // initialize class data
-        initDataQueues();
-        mDProc = new DataSampleProcessor();
-
+        initSensorMessages();
+        // initialize all Qs for data
+        initAllDataQ();
+        // init all sensors to start getting data
+        initAllSensors();
         // Enables Always-on
         setAmbientEnabled();
-        Log.d(TAG, "My message: on create");
-        getAccelerometerData();
-        getGyroscopeData();
-        getGravityData();
-        getHRData();
     }
 
-    private void initDataQueues(){
-        mAccXQ = EvictingQueue.create(Q_SZ);
-        mAccYQ = EvictingQueue.create(Q_SZ);
-        mAccZQ = EvictingQueue.create(Q_SZ);
-        mGyrXQ = EvictingQueue.create(Q_SZ);
-        mGyrYQ = EvictingQueue.create(Q_SZ);
-        mGyrZQ = EvictingQueue.create(Q_SZ);
-        mGvtXQ = EvictingQueue.create(Q_SZ);
-        mGvtYQ = EvictingQueue.create(Q_SZ);
-        mGvtZQ = EvictingQueue.create(Q_SZ);
+    private void initSensorMessages(){
+        sensorMessagePrefix = new HashMap();
+        sensorMessages = new HashMap();
+        sensorMessagePrefix.put(Sensor.TYPE_ACCELEROMETER, "Acc: ");
+        sensorMessages.put(Sensor.TYPE_ACCELEROMETER, sensorMessagePrefix.get(Sensor.TYPE_ACCELEROMETER));
+
+        sensorMessagePrefix.put(Sensor.TYPE_GYROSCOPE, "Gyr: ");
+        sensorMessages.put(Sensor.TYPE_GYROSCOPE, sensorMessagePrefix.get(Sensor.TYPE_GYROSCOPE));
+
+        sensorMessagePrefix.put(Sensor.TYPE_GRAVITY, "Gra: ");
+        sensorMessages.put(Sensor.TYPE_GRAVITY, sensorMessagePrefix.get(Sensor.TYPE_GRAVITY));
+
+        sensorMessagePrefix.put(Sensor.TYPE_HEART_RATE, "HR: ");
+        sensorMessages.put(Sensor.TYPE_HEART_RATE, sensorMessagePrefix.get(Sensor.TYPE_HEART_RATE));
     }
 
-    private void getAccelerometerData() {
-        SensorManager mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
-        Sensor mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        if(mAccelerometerSensor != null){
-            Log.d(TAG, "*********** Accelerometer found!");
+    private void initAllDataQ(){
+        allDataQ = new Vector<EvictingQueue<Float>>();
+        sensorDataIndex = new HashMap();
+        sensorDataIndex.put(Sensor.TYPE_ACCELEROMETER, new Pair<Integer, Integer>(0, 3));
+        for(int i=0; i<3; i++) {
+            allDataQ.add(EvictingQueue.<Float>create(Q_SZ));
         }
-        mSensorManager.registerListener((SensorEventListener) this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
+        sensorDataIndex.put(Sensor.TYPE_GYROSCOPE, new Pair<Integer, Integer>(3, 3));
+        for(int i=0; i<3; i++) {
+            allDataQ.add(EvictingQueue.<Float>create(Q_SZ));
+        }
+        sensorDataIndex.put(Sensor.TYPE_GRAVITY, new Pair<Integer, Integer>(6, 3));
+        for(int i=0; i<3; i++) {
+            allDataQ.add(EvictingQueue.<Float>create(Q_SZ));
+        }
+        sensorDataIndex.put(Sensor.TYPE_HEART_RATE, new Pair<Integer, Integer>(9, 1));
+        allDataQ.add(EvictingQueue.<Float>create(Q_SZ));
     }
 
-    private void getGyroscopeData() {
-        SensorManager mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
-        Sensor mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
-        if(mAccelerometerSensor != null){
-            Log.d(TAG, "*********** Gyroscope found!");
-        }
-        mSensorManager.registerListener((SensorEventListener) this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
+    private void updateSensorMessage(int sensorType, String message){
+        sensorMessages.put(sensorType, sensorMessagePrefix.get(sensorType) + message);
     }
 
-    private void getGravityData() {
-        SensorManager mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
-        Sensor mGravitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-
-        if(mGravitySensor != null){
-            Log.d(TAG, "*********** Gravity sensor found!");
+    private void registerSensor(int sensorType, int delay){
+        SensorManager sensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
+        Sensor sensor = sensorManager.getDefaultSensor(sensorType);
+        if(sensor != null){
+            Log.d(TAG, "*********** !" + sensorType);
         }
-        mSensorManager.registerListener((SensorEventListener) this, mGravitySensor, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener((SensorEventListener) this, sensor, delay);
     }
 
-    private void getHRData() {
-        SensorManager mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
-        Sensor mHRSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
-
-        if(mHRSensor != null){
-            Log.d(TAG, "*********** HR sensor found!");
-        }
-        mSensorManager.registerListener((SensorEventListener) this, mHRSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    private void initAllSensors(){
+        registerSensor(Sensor.TYPE_ACCELEROMETER, SensorManager.SENSOR_DELAY_GAME);
+        registerSensor(Sensor.TYPE_GYROSCOPE, SensorManager.SENSOR_DELAY_GAME);
+        registerSensor(Sensor.TYPE_GRAVITY, SensorManager.SENSOR_DELAY_GAME);
+        registerSensor(Sensor.TYPE_GRAVITY, SensorManager.SENSOR_DELAY_GAME);
+        registerSensor(Sensor.TYPE_HEART_RATE, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     private String currentTimeStr() {
@@ -109,59 +106,29 @@ public class MainActivity extends WearableActivity implements SensorEventListene
         return df.format(c.getTime());
     }
 
-    @SuppressLint("DefaultLocale")
-    private String handleAccData(float values[]){
-        mAccXQ.add(values[0]);
-        mAccYQ.add(values[1]);
-        mAccZQ.add(values[2]);
-
-        Vector<Float> stats = mDProc.calcMean(mAccXQ);
-        String mMesgAcc;
-        mMesgAcc = String.format("Acc: %.2f, %.2f, %.2f, %.2f",
-                                        values[0],
-                                        values[1],
-                                        values[2],
-                                        stats.elementAt(0));
-        return mMesgAcc;
-    }
-
-    private String handleGyrData(float values[]){
-        String mMesgGyr = "Gyr: " + String.format("%.1f", values[0]) + "," + String.format("%.1f", values[1]) + "," + String.format("%.1f", values[2]);
-        mGyrXQ.add(values[0]);
-        mGyrYQ.add(values[1]);
-        mGyrZQ.add(values[2]);
-        return mMesgGyr;
-    }
-
-    private String handleGvtData(float values[]){
-        String mMesgGra = "Gra: " + String.format("%.1f", values[0]) + "," + String.format("%.1f", values[1]) + "," + String.format("%.1f", values[2]);
-        mGvtXQ.add(values[0]);
-        mGvtYQ.add(values[1]);
-        mGvtZQ.add(values[2]);
-        return mMesgGra;
+    private void handleSensorData(int sensorType, float values[]) {
+        Pair<Integer, Integer> indNum = (Pair<Integer, Integer>) sensorDataIndex.get(sensorType);
+        int startInd = indNum.first.intValue();
+        String dataString = "";
+        for(int i=0; i<indNum.second.intValue(); i++){
+            allDataQ.elementAt(startInd + i).add(values[i]);
+            dataString += String.format("%.2f, ", values[i]);
+        }
+        updateSensorMessage(sensorType, dataString);
     }
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            mMesgAcc = handleAccData(event.values);
-        }
-        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            mMesgGyr = handleGyrData(event.values);
-        }
-        if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
-            mMesgGra = handleGvtData(event.values);
-        }
-        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
-            mMesgHR = "HR: " + String.format("%d", (int)event.values[0]);
-        }
-
-        String msg = mMesgAcc + "\n" + mMesgGyr + "\n" + mMesgGra + "\n" + mMesgHR;
-        mTextView.setText(msg);
+        handleSensorData(event.sensor.getType(), event.values);
+        String viewString = (String) sensorMessages.get(Sensor.TYPE_ACCELEROMETER) + "\n" +
+                            (String) sensorMessages.get(Sensor.TYPE_GYROSCOPE) + "\n" +
+                            (String) sensorMessages.get(Sensor.TYPE_GRAVITY) + "\n" +
+                            (String) sensorMessages.get(Sensor.TYPE_HEART_RATE);
+        Log.d(TAG, "screen string = " + viewString);
+        mTextView.setText(viewString);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 }
